54a96d74a46eaf1e23276ae99b78de4bc8d70395
Rob Bridges <Rmb101187@gmail.com>
2022-06-27 10:48:57 -0700

    Linked list broke man's sorting done

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import { Sorter } from "./sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList} from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([10,3,-5,0,3782979]);
-const charactersCollection = new CharactersCollection('ayrhalksdhhu'); 
+// const numbersCollection = new NumbersCollection([10,3,-5,0,3782979]);
+// const charactersCollection = new CharactersCollection('ayrhalksdhhu'); 
 
-const sorter = new Sorter(charactersCollection);
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
